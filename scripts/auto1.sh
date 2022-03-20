f049aae675f34a784cf7025de188aadfe41448de
Yana Ryabtseva <yana@ryabtsev.pro>
2022-03-20 16:21:10 +0300

    hello2

---
+++ scripts/auto1.sh
@@ -0,0 +1,6 @@
+#!/usr/bin/bash
+set -e
+mkdir -p /c/qa/git/first/auto
+touch /c/qa/git/first/auto/auto2.txt
+echo "hello world" >> /c/qa/git/first/auto/auto2.txt 
+echo  "love you" >> /c/qa/git/first/auto/auto2.txt
---
+++ scripts/auto2.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/bash
+
+set -e
+mkdir -p ../auto2
+touch ../auto2/file2.txt
+echo "hello world 2" >> ../auto2/file2.txt
+
+echo "work"
+
